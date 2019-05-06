4a4b8a89a92bd2a2e78a202891648ab0bd429466
blaws006 <blaws006@gmail.com>
2019-05-05 21:11:05 -0400

    Removing the date

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, lng, callback) => {
     } else {
       callback(
         undefined,
-        `${new Date(body.currently.time)}. ${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Today's high will be ${body.daily.data[0].temperatureHigh} and the low will be ${body.daily.data[0].temperatureLow}.`,
+        `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Today's high will be ${body.daily.data[0].temperatureHigh} and the low will be ${body.daily.data[0].temperatureLow}.`,
       );
     }
   });
