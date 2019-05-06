2c4ee8c7ed1f7c2c47de8397eccb1a547f853d0f
blaws006 <blaws006@gmail.com>
2019-05-05 21:09:14 -0400

    Add forecast information for app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,16 @@ const request = require('request');
 const forecast = (lat, lng, callback) => {
   const url = `https://api.darksky.net/forecast/86760d9e7ddfd7d985e43e6f02effeed/${lat},${lng}`;
 
-  request({ url, json: true }, (error, {body}) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service', undefined);
     } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
-      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability }% chance of rain.`);
+      callback(
+        undefined,
+        `${new Date(body.currently.time)}. ${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Today's high will be ${body.daily.data[0].temperatureHigh} and the low will be ${body.daily.data[0].temperatureLow}.`,
+      );
     }
   });
 };
